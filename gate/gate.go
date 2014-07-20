046b8f52b923f91c938e42500c34a137642788d3
Michael Crilly <mrcrilly@gmail.com>
2014-07-20 21:43:51 +0100

    Adding OTPConfig{} for reference only.

--- gate/gate.go
+++ gate/gate.go
@@ -16,6 +16,14 @@ const (
     GATE_HOTP_COUNTER = 0
 )
 
+// type OTPConfig struct {
+//     Secret string // 80-bit base32 encoded string of the user's secret
+//     WindowSize int // valid range: technically 0..100 or so, but beyond 3-5 is probably bad security
+//     HotpCounter int // the current otp counter. 0 if the user uses time-based codes instead.
+//     DisallowReuse []int // timestamps in the current window unavailable for re-use
+//     ScratchCodes []int // an array of 8-digit numeric codes that can be used to log in
+// }
+
 type Gate struct {
     UserID string 
     UserSecret string 
